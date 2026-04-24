4b0399b2d890124b6371d11dcd9cd5b6d9ec392f
devnagar-ai <vedevnagar@gmail.com>
2026-04-24 11:52:35 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,15 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Closing parentheses")
+Closing parentheses
+>>> print(10)
+10
+>>> print(10+20)
+30
+>>> a = 10
+>>> b = 20
+>>> c = a + b
+>>> print(c)
+30
+>>> c
+30
